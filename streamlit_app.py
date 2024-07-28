68c9362dcc45d5d213e81697f23d615cccf8f8fb
MBallet <87940847+MBallet@users.noreply.github.com>
2024-07-28 12:11:33 -0500

    Update streamlit_app.py
    Changed title, added some text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Keyword Cluster-er")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Reference: [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("Do you have a keyword list?")
+
